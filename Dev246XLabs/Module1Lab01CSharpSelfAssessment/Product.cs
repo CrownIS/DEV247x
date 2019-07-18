93aba3970d6fcf37aa1ba046a1e8d2060dc4df19
Steve Miller <Steve.Miller@ssigroup.com>
2019-07-18 14:28:01 -0500

    Added module 1 lab 01 self assessment.

---
+++ Dev246XLabs/Module1Lab01CSharpSelfAssessment/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Module1Lab01CSharpSelfAssessment
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Price: {Price}";
+        }
+    }
+}
---
+++ Dev246XLabs/Module1Lab01CSharpSelfAssessment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Module1Lab01CSharpSelfAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Serialize
+            var product1 = new Product()
+            {
+                ID = 1,
+                Name = "Laptop",
+                Price = 1024.22
+            };
+
+            var json = JsonConvert.SerializeObject(product1);
+            Console.WriteLine("Product (Json):");
+            Console.WriteLine(json);
+
+            // Deserialize
+            var product2 = JsonConvert.DeserializeObject(json, typeof(Product));
+            Console.WriteLine("\nProduct (object):");
+            Console.WriteLine(product2.ToString());
+        }
+    }
+}
